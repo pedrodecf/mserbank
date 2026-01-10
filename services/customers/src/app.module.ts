1a9d1865433ac9b3f1117b4d406ab2523a2d0f9c
Pedro de Freitas <contato.pedrodecf@gmail.com>
2026-01-10 12:13:27 -0300

    T02d: feat - add DatabaseModule and PrismaService for database integration

--- services/customers/src/app.module.ts
+++ services/customers/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from './infrastructure/database/database.module';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [DatabaseModule, UsersModule],
 })
 export class AppModule {}
